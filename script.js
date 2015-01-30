6e3448cccf0498bde52be2381be896c9104c58bb
Jesse the Game <jesse@jessethegame.net>
2015-01-30 14:56:34 +0000

    Add marker numbers and ratings

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
+function randomRatings(id) {
+  var ratings, total, i;
+
+  ratings = [
+    ['★★★★★', Math.floor(Math.random() * 10)],
+    ['★★★★☆', Math.floor(Math.random() * 10)],
+    ['★★★☆☆', Math.floor(Math.random() * 10)],
+    ['★★☆☆☆', Math.floor(Math.random() * 10)],
+    ['★☆☆☆☆', Math.floor(Math.random() * 10)],
+  ];
+
+  total = 0;
+  ratings.forEach(function (rating) {
+    total += rating[1];
+  })
+  ratings.total = total;
+  return ratings;
+}
+
 function drawChart(marker) {
 
   // Create the data table.
   var data = new google.visualization.DataTable();
-  data.addColumn('string', 'Topping');
-  data.addColumn('number', 'Slices');
-  data.addRows([
-    ['Mushrooms', 3],
-    ['Onions', 1],
-    ['Olives', 1],
-    ['Zucchini', 1],
-    ['Pepperoni', 2]
-  ]);
+  data.addColumn('string', 'Rating');
+  data.addColumn('number', 'Count');
+  console.log(marker);
+  data.addRows(marker.ratings);
 
   // Set chart options
-  var options = {'title':'Pizza sold @ '+
-                         marker.getPosition().toString(),
-                 'width':400,
-                 'height':150};
+  var options = {
+    title: 'Ratings for <restaurant_name>',
+    width: 400,
+    height: 150,
+    colors: [
+      '#99ff99',
+      '#bbff99',
+      '#ffff99',
+      '#ffbb99',
+      '#ff9999',
+    ]
+  };
 
   var node        = document.createElement('div'),
       infoWindow  = new google.maps.InfoWindow(),
@@ -29,104 +51,53 @@ function drawChart(marker) {
 google.load('visualization', '1.0', {'packages':['corechart']});
 
 
-// function initialize() {
-
-//     var mapOptions = {
-//       center: new google.maps.LatLng(-33.891044,151.275537),
-//       zoom: 10,
-//       mapTypeId: google.maps.MapTypeId.ROADMAP
-//     };
-
-//     var map = new google.maps.Map(document.getElementById("map_canvas"),
-//         mapOptions);
-
-//     var marker1 = new google.maps.Marker({
-//         position: mapOptions.center,
-//         map: map
-//     });
-
-//     google.maps.event.addListener(marker1, 'click', function() {
-//       drawChart(this);
-//     });
-
-//   }
-
-
-
 var marker;
 var gm_map;
 var markerArray = [];
-var address = 'Sweden';
-var geocoder = new google.maps.Geocoder();
+// var address = 'Sweden';
+// var geocoder = new google.maps.Geocoder();
 
-geocoder.geocode({ 'address': address }, function(results, status) {
-    if(status == google.maps.GeocoderStatus.OK) {
-        gm_map.setCenter(results[0].geometry.location);
-        gm_map.fitBounds(results[0].geometry.bounds);
-    } else {
-        alert("Unable to complete the geological setting due to the following error:\n\n" + status);
-    }
-});
+// geocoder.geocode({ 'address': address }, function(results, status) {
+//     if(status == google.maps.GeocoderStatus.OK) {
+//         gm_map.setCenter(results[0].geometry.location);
+//         gm_map.fitBounds(results[0].geometry.bounds);
+//     } else {
+//         alert("Unable to complete the geological setting due to the following error:\n\n" + status);
+//     }
+// });
 
 
+function icon(count) {
+  return 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=' + count + '|ff9999|000000';
+}
+
+function randomCoord() {
+  return new google.maps.LatLng(Math.random(), Math.random());
+}
 
 function initialize() {
-    var marker, i;
-    var clusterMarkers = [
-        new google.maps.Marker({
-            position: new google.maps.LatLng(59.381059,13.504026),
-            map: gm_map,
-            title:"P1220214 1.JPG"
-        }),
+    var clusterMarkers, i, markers, ratings, lat, lng;
 
-        new google.maps.Marker({
-            position: new google.maps.LatLng(59.338683,13.492057),
-            map: gm_map,
-            title:"P1220214 2.JPG"
-        }),
+    clusterMarkers = [];
 
-        new google.maps.Marker({
-            position: new google.maps.LatLng(59.340715,13.49631),
-            map: gm_map,
-            title:"P1220214 3.JPG"
-        }),
-
-        new google.maps.Marker({
-            position: new google.maps.LatLng(59.327232,13.487384),
-            map: gm_map,
-            title:"P1220214 4.JPG"
-        }),
-
-        new google.maps.Marker({
-            position: new google.maps.LatLng(59.379034,13.516566),
-            map: gm_map,
-            title:"P1220214 5.JPG"
-        }),
-
-        new google.maps.Marker({
-            position: new google.maps.LatLng(59.328631,13.485688),
-            map: gm_map,
-            title:"P1220214 6.JPG"
-        }),
-
-        new google.maps.Marker({
-            position: new google.maps.LatLng(59.328657,13.485591),
-            map: gm_map,
-            title:"P1220214 7.JPG"
-        }),
-
-        new google.maps.Marker({
-            position: new google.maps.LatLng(59.328501,13.485782),
-            map: gm_map,
-            title:"P1220214 8.JPG"
-        })
-        ]
+    for (var i = 0; i < 10; i++) {
+      ratings = randomRatings();
+      clusterMarkers.push(new google.maps.Marker({
+          position: randomCoord(),
+          map: gm_map,
+          icon: icon(ratings.total),
+          title: "Marker " + i,
+          ratings: ratings,
+      }));
+    }
 
     var options_googlemaps = {
         minZoom: 4,
         maxZoom: 18,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
-        streetViewControl: false
+        streetViewControl: false,
+        center: new google.maps.LatLng(0, 0),
+        zoom: 6,
     }
 
     gm_map = new google.maps.Map(document.getElementById('google-maps'), options_googlemaps);
@@ -137,8 +108,6 @@ function initialize() {
         maxZoom: 18,
         zoomOnClick: false
     };
-
-
 
     var markerCluster = new MarkerClusterer(gm_map, clusterMarkers, options_markerclusterer);
 
@@ -167,17 +136,8 @@ function initialize() {
 
 
     for(i = 0; i < clusterMarkers.length; i++) {
-       var marker = clusterMarkers[i];
-
-        google.maps.event.addListener(marker, 'click', (function(marker) {
-            return function() {
-                $('#toggle-photolist').fadeIn();
-                $('#close-overlay').fadeIn();
-                $('#view-multiblephotos').hide();
-                $('#list-photos').hide();
-                $('#view-singlephoto').show().html(marker.getTitle());
-            }
-        })(marker));
+      var marker = clusterMarkers[i];
+      google.maps.event.addListener(marker, 'click', drawChart.bind(null, marker));
     }
 }
 
